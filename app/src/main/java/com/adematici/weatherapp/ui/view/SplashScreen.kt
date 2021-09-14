0d895179d73d5186603f499b267bcaae02d75a37
adematici <adematicis41@gmail.com>
2021-09-14 23:00:13 +0300

    SplashScreen.kt completed

--- app/src/main/java/com/adematici/weatherapp/ui/view/SplashScreen.kt
+++ app/src/main/java/com/adematici/weatherapp/ui/view/SplashScreen.kt
@@ -1,9 +1,43 @@
 package com.adematici.weatherapp.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.adematici.weatherapp.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
+
+    LaunchedEffect(key1 = true) {
+        delay(1500)
+        navController.navigate("main_screen") {
+            popUpTo("splash_screen") { inclusive = true }
+        }
+    }
+    
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.splash_image),
+                contentDescription = "splash_image",
+                modifier = Modifier.size(300.dp)
+            )
+        }
+    }
 
 }
