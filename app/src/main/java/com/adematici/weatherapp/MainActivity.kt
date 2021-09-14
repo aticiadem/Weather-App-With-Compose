98d903c26b732ed13fa2ce79fd84c4752e89d075
adematici <adematicis41@gmail.com>
2021-09-14 22:48:03 +0300

    navigation and screens created

--- app/src/main/java/com/adematici/weatherapp/MainActivity.kt
+++ app/src/main/java/com/adematici/weatherapp/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.adematici.weatherapp.ui.theme.WeatherAppTheme
+import com.adematici.weatherapp.ui.view.MainScreen
+import com.adematici.weatherapp.ui.view.SplashScreen
+import com.adematici.weatherapp.ui.view.WeatherScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,9 +23,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Navigation()
                 }
             }
         }
@@ -27,14 +32,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "splash_screen"
+    ) {
+        composable("splash_screen") {
+            SplashScreen(navController)
+        }
+        composable("main_screen") {
+            MainScreen(navController)
+        }
+        composable(
+            "weather_screen/{city}",
+            arguments = listOf(
+                navArgument("city") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            WeatherScreen(
+                navController,
+                city = backStackEntry.arguments?.getString("city")
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/adematici/weatherapp/ui/view/SplashScreen.kt
@@ -0,0 +1,9 @@
+package com.adematici.weatherapp.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SplashScreen(navController: NavController) {
+
+}
