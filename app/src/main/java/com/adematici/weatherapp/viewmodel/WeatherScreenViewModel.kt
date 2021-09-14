379c2d3f149bff45f182046867180978ceb8968c
Adem ATİCİ <58858983+aticiadem@users.noreply.github.com>
2021-09-14 22:16:34 +0300

    project updated and viewmodel created (#6)

---
+++ app/src/main/java/com/adematici/weatherapp/viewmodel/WeatherScreenViewModel.kt
@@ -0,0 +1,19 @@
+package com.adematici.weatherapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.adematici.weatherapp.model.Weather
+import com.adematici.weatherapp.repository.WeatherRepository
+import com.adematici.weatherapp.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherScreenViewModel @Inject constructor(
+    private val repository: WeatherRepository
+) : ViewModel() {
+
+    suspend fun getWeatherData(city: String) : Resource<Weather> {
+        return repository.getWeatherData(city)
+    }
+
+}
