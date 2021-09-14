c881199dbe9dc9e3192c2b4bc9a56015cb4b537f
Adem ATİCİ <58858983+aticiadem@users.noreply.github.com>
2021-09-14 21:11:55 +0300

    dependency injection created (#2)

---
+++ app/src/main/java/com/adematici/weatherapp/Application.kt
@@ -0,0 +1,8 @@
+package com.adematici.weatherapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application() {
+}
--- app/src/main/java/com/adematici/weatherapp/MainActivity.kt
+++ app/src/main/java/com/adematici/weatherapp/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.adematici.weatherapp.ui.theme.WeatherAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
