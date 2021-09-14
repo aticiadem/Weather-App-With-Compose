ecec89c53e66a0f78db02b0a1b1ddc769ec388e6
Adem ATİCİ <58858983+aticiadem@users.noreply.github.com>
2021-09-14 22:49:32 +0300

    navigation and screens created (#7)

---
+++ app/src/main/java/com/adematici/weatherapp/ui/view/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.adematici.weatherapp.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreen(navController: NavController) {
+
+}
---
+++ app/src/main/java/com/adematici/weatherapp/ui/view/WeatherScreen.kt
@@ -0,0 +1,12 @@
+package com.adematici.weatherapp.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun WeatherScreen(
+    navController: NavController,
+    city: String?
+) {
+
+}
