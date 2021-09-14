d99696e94dbc2222b197815ea0a312f681039756
adematici <adematicis41@gmail.com>
2021-09-14 21:34:51 +0300

    service created

---
+++ app/src/main/java/com/adematici/weatherapp/service/WeatherAPI.kt
@@ -0,0 +1,17 @@
+package com.adematici.weatherapp.service
+
+import com.adematici.weatherapp.model.Weather
+import com.adematici.weatherapp.util.Constants.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherAPI {
+
+    @GET("data/2.5/weather")
+    suspend fun getWeatherData(
+        @Query("q") city : String,
+        @Query("units") units: String = "metric",
+        @Query("appid") appId: String = API_KEY // your api key
+    ) : Weather
+
+}
---
+++ app/src/main/java/com/adematici/weatherapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.adematici.weatherapp.util
+
+object Constants {
+
+    const val BASE_URL = "https://api.openweathermap.org/"
+    const val API_KEY = "f222dbc4334d608a51b8455e6ab0af4a"
+
+}
