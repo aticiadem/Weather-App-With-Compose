f9874f38480244b0702a3a8f23d6e1cbb63ca259
adematici <adematicis41@gmail.com>
2021-09-14 22:15:15 +0300

    project updated and viewmodel created

--- app/src/main/java/com/adematici/weatherapp/repository/WeatherRepository.kt
+++ app/src/main/java/com/adematici/weatherapp/repository/WeatherRepository.kt
@@ -2,13 +2,23 @@ package com.adematici.weatherapp.repository
 
 import com.adematici.weatherapp.model.Weather
 import com.adematici.weatherapp.service.WeatherAPI
+import com.adematici.weatherapp.util.Resource
 import dagger.hilt.android.scopes.ActivityScoped
+import java.lang.Exception
+import javax.inject.Inject
 
 @ActivityScoped
-class WeatherRepository(private val api: WeatherAPI) {
+class WeatherRepository @Inject constructor(
+    private val api: WeatherAPI
+) {
 
-    suspend fun getWeatherData(city: String): Weather {
-        return api.getWeatherData(city)
+    suspend fun getWeatherData(city: String): Resource<Weather> {
+        val response = try {
+            api.getWeatherData(city = city)
+        } catch (e: Exception){
+            return Resource.Error("Try Again.")
+        }
+        return Resource.Success(response)
     }
 
 }
