ee82c212d1c2fb93f1d2c992ee3dbaba2e2c2721
adematici <adematicis41@gmail.com>
2021-09-15 12:48:45 +0300

    animation added to WeatherScreen.kt

--- app/src/main/java/com/adematici/weatherapp/ui/view/WeatherScreen.kt
+++ app/src/main/java/com/adematici/weatherapp/ui/view/WeatherScreen.kt
@@ -1,12 +1,18 @@
 package com.adematici.weatherapp.ui.view
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.produceState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,6 +27,7 @@ import com.adematici.weatherapp.model.Weather
 import com.adematici.weatherapp.util.Resource
 import com.adematici.weatherapp.viewmodel.WeatherScreenViewModel
 
+@ExperimentalAnimationApi
 @ExperimentalCoilApi
 @Composable
 fun WeatherScreen(
@@ -42,7 +49,14 @@ fun WeatherScreen(
         }
     ) {
         Box(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = 20.dp,
+                    bottom = 20.dp,
+                    start = 25.dp,
+                    end = 25.dp
+                ),
             contentAlignment = Alignment.Center
         ) {
             when (weatherData) {
@@ -60,18 +74,22 @@ fun WeatherScreen(
     }
 }
 
+@ExperimentalAnimationApi
 @ExperimentalCoilApi
 @Composable
 fun WeatherCard(
     weather: Weather
 ) {
     val imageUrl = "https://openweathermap.org/img/wn/${weather.weather[0].icon}.png"
+    var expanded by remember { mutableStateOf(false) }
+    var cardHeight by remember { mutableStateOf(300) }
 
     Card(
         border = BorderStroke(3.dp, Color.Red),
         shape = RoundedCornerShape(20.dp),
         modifier = Modifier
-            .size(350.dp)
+            .fillMaxWidth()
+            .height(cardHeight.dp)
             .clip(RoundedCornerShape(20.dp))
             .padding(10.dp)
     ) {
@@ -97,6 +115,29 @@ fun WeatherCard(
                     image = R.drawable.temperature,
                     description = weather.main.temp.toString()
                 )
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Icon(
+                imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                contentDescription = "Arrow",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clickable {
+                        expanded = !expanded
+                        if (cardHeight == 300) cardHeight = 400 else cardHeight = 300
+                    }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            AnimatedVisibility(visible = expanded) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 20.dp)
+                ) {
+                    WeatherDetailRow("Minimum Temperature: ", weather.main.tempMin.toString())
+                    WeatherDetailRow("Maximum Temperature: ", weather.main.tempMax.toString())
+                    WeatherDetailRow("Pressure: ", weather.main.pressure.toString())
+                }
             }
         }
     }
@@ -129,6 +170,21 @@ fun WeatherDataInfoRow(
             modifier = Modifier.size(50.dp)
         )
         Spacer(modifier = Modifier.height(5.dp))
+        Text(text = description)
+    }
+}
+
+@Composable
+fun WeatherDetailRow(
+    text: String,
+    description: String
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = text)
         Text(text = description)
     }
 }
